7ff4830fd4c9696f42474d1f65767c189d3d8152
Brian W Bush <code@functionally.io>
2021-08-01 18:18:46 -0600

    CARD-73 skeleton

---
+++ shell.nix
@@ -0,0 +1,25 @@
+let
+  project = import ./default.nix;
+in
+  project.shellFor {
+
+    packages = ps: with ps; [
+      plutus-onchain
+    ];
+
+    # Set the following to `false` do disable the lengthy building of documentation.
+    withHoogle = false;
+
+     # See overlays/tools.nix for more details
+    tools = {
+      cabal                   = "latest";
+      haskell-language-server = "latest";
+      hlint                   = "latest";
+      pointfree               = "latest";
+    # pointfull               = "latest";
+    };
+
+    buildInputs = [ (import <nixpkgs> {}).git ];
+
+    exactDeps = true;
+  }
