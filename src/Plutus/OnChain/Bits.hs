c2b8f271e46a2f130694c13404bfd3298b3b5a4e
Brian W Bush <code@functionally.io>
2021-08-02 18:41:54 -0600

    refactored bit operations out of random module

---
+++ src/Plutus/OnChain/Bits.hs
@@ -0,0 +1,54 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  $Headers
+-- Copyright   :  (c) 2021 Brian W Bush
+-- License     :  MIT
+--
+-- Maintainer  :  Brian W Bush <code@functionally.io>
+-- Stability   :  Experimental
+-- Portability :  Portable
+--
+-- | Bit operations.
+--
+-----------------------------------------------------------------------------
+
+
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module Plutus.OnChain.Bits (
+-- * Functions
+  xor
+, even
+) where
+
+
+import PlutusTx.Prelude hiding (even)
+
+
+{-# INLINABLE xor #-}
+
+-- | Compute the exclusive-or of two non-negative integers.
+xor :: Integer -- ^ The first integer.
+    -> Integer -- ^ The second integer.
+    -> Integer -- ^ The bitwise exclusive-or.
+xor = xor' 1 0
+  where
+    xor' :: Integer
+         -> Integer
+         -> Integer
+         -> Integer
+         -> Integer
+    xor' m z x y
+      | x == 0           = z + m * y
+      | y == 0           = z + m * x
+      | even x == even y = xor' (2 * m)  z      (x `divide` 2) (y `divide` 2) 
+      | otherwise        = xor' (2 * m) (z + m) (x `divide` 2) (y `divide` 2)
+
+
+{-# INLINABLE even #-}
+
+-- | Test whether a non-negative integer is even.
+even :: Integer -- ^ The integer.
+     -> Bool    -- ^ Whether the integer is even.
+even x = x `modulo` 2 == 0
